ac3a6fddc1d3ce0560b5d600b1ad57f8a06d510d
Yunus Fırat <ynssfrt@gmail.com>
2026-02-24 22:31:24 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ return (
+ <div>
+ <h1>Web Tasarimi ve Programlama </h1>
+ <h2>LAB -1</h2>
+ <p>Ad Soyad: [Kendi Adin]</p>
+ <p>Ogrenci No: [Numaran]</p>
+ </div>
+ )
+}
+
+export default App
