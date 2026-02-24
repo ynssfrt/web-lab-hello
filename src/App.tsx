a1055bde69a43fd32a0ae4096f188935658eca5a
Yunus Fırat <ynssfrt@gmail.com>
2026-02-24 23:34:35 +0300

    feat: 'Iletisim' section added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
   return (
     <> {/* Tüm elemanları saran kapsayıcı tag */}
+  <a href="#main -content" className="skip -link">
+  Ana icerige atla
+  </a>
+
       <header>
         <nav aria-label="Ana navigasyon">
           <ul>
@@ -32,7 +36,44 @@ function App() {
 
         <section id="iletisim">
           <h2>Iletisim</h2>
-          {/* Form Uygulama-4 kodları buraya gelecek */}
+          { 
+ <form action="#" method="POST" noValidate> {/* novalidate -> noValidate oldu */}
+  <fieldset>
+    <legend>Iletisim Formu</legend>
+    
+    <div className="form-group"> {/* class -> className oldu */}
+      <label htmlFor="name">Ad Soyad: </label> {/* for -> htmlFor oldu */}
+      <input type="text" id="name" name="name" required minLength={2} aria-describedby="name-error" />
+      <small id="name-error" className="error-msg" role="alert"></small>
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="email">E-posta: </label>
+      <input type="email" id="email" name="email" required aria-describedby="email-error" />
+      <small id="email-error" className="error-msg" role="alert"></small>
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="subject">Konu: </label>
+      <select id="subject" name="subject" required aria-describedby="subject-error">
+        <option value="">-- Seciniz --</option>
+        <option value="is">Is Teklifi</option>
+        <option value="soru">Soru</option>
+        <option value="oneri">Oneri</option>
+      </select>
+      <small id="subject-error" className="error-msg" role="alert"></small>
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="message">Mesajiniz:</label>
+      <textarea id="message" name="message" rows={5} required minLength={10} aria-describedby="message-error"></textarea> {/* rows="5" -> rows={5} oldu */}
+      <small id="message-error" className="error-msg" role="alert"></small>
+    </div>
+
+    <button type="submit">Gonder</button>
+  </fieldset>
+</form>
+}
         </section>
       </main>
 
